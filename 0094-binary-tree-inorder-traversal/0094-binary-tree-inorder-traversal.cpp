6b618817a6e42d61b7cbaaf22a329133a18922eb
Mohit Dwivedi <118814602+DarkImpact1@users.noreply.github.com>
2025-03-06 20:07:44 +0530

    Time: 0 ms (100%), Space: 10.7 MB (88.35%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,17 +11,29 @@
  */
 class Solution {
 public:
-void inOrder(TreeNode* root, vector<int>&ans){
-    if(!root) return;
-
-    inOrder(root->left, ans);
-    ans.push_back(root->val);
-    inOrder(root->right,ans);
-}
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        if(!root) return ans;
-        inOrder(root,ans);
-        return ans;
+        vector<int> answer;
+        if(!root) return answer;
+        TreeNode* cur = root;
+        while(cur){
+            if(!cur->left) {
+                answer.push_back(cur->val); // if does not have left push the answer
+                cur = cur->right; // move to right
+            }else{
+                TreeNode *left = cur->left;
+                while(left->right && left->right != cur){
+                    left = left->right; // move to the rightest node of left subtree
+                }
+                if(left -> right == nullptr){ 
+                    left->right = cur; // connect the thread
+                    cur = cur->left; // then move to the left
+                }else{
+                    left->right = nullptr; // remove the thread
+                    answer.push_back(cur->val); // after removing thread -> add the answer
+                    cur = cur->right; // move to right
+                }
+            }
+        }
+        return answer;
     }
 };
